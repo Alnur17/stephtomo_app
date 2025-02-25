29bfd61c84cba723ecc93e0aa18fc43d92a4da41
Sultan Md. Alnur <98782302+Alnur17@users.noreply.github.com>
2025-02-25 18:42:28 +0600

    some modification made

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:get_storage/get_storage.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   runApp(
     GetMaterialApp(
